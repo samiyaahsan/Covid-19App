e059f905a70ca54fdedc3c006e75e40c458730df
samiyaahsan <76249372+samiyaahsan@users.noreply.github.com>
2020-12-30 17:26:16 +0500

    Update Country.js

--- Country.js
+++ Country.js
@@ -4,7 +4,6 @@ import './country.css';
 const Country = ({stats}) => {
     return(
         <div className='country'>
-            <img src={`https://www.countryflags.io/${stats.CountryCode}/flat/64.png`} alt={stats.Country}></img>
             <h2>{stats.Country}</h2>
             <div className='describe'>
                 <p>{`Active : ${stats.Active}`}</p> 
