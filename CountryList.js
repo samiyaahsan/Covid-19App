0cf26603bffe6bfd8ea3de8afb793901bab6b2e7
samiyaahsan <76249372+samiyaahsan@users.noreply.github.com>
2020-12-30 17:26:55 +0500

    Update CountryList.js

--- CountryList.js
+++ CountryList.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Country from '../Country/Country';
-import './countrylist.css';
 const CountryList = ({stats}) =>{
     return(
         <div className='countrylist'>
